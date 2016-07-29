1596cb715cdf8d6e93cfeaebba9be1adb7729ba3
rvkrish <vramisetti@partner.aligntech.com>
2016-07-29 23:22:05 +0530

    removed course cost

--- db/migrate/20160405111422_add_course_cost_to_course.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseCostToCourse < ActiveRecord::Migration
-  def change
-    add_column :courses, :course_cost, :float
-  end
-end
--- db/migrate/20160727194922_create_courses.rb
+++ db/migrate/20160727194922_create_courses.rb
@@ -3,7 +3,7 @@ class CreateCourses < ActiveRecord::Migration
     create_table :courses do |t|
       t.string :course_name
       t.string :course_description
-
+      t.float :course_cost
       t.timestamps null: false
     end
   end
