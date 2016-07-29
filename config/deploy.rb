d1078edc19a767b9846cf352624a43971d919090
rvkrish <vramisetti@partner.aligntech.com>
2016-07-30 02:19:52 +0530

    ngnix configured

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
 # config valid only for current version of Capistrano
 lock '3.6.0'
 
+
+
 # Change these
 server '139.59.24.205', port: 22 , roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:rvkrish/mehub-social.git'
 set :application,     'me_hub'
 set :user,            'root'
-
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
@@ -15,8 +18,15 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -29,6 +39,17 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
 
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
@@ -42,6 +63,20 @@ namespace :deploy do
     end
   end
 
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
 
   before :starting,     :check_revision
   after  :finishing,    :compile_assets
